10c4a4a8a529d0aa1ef782ea60a4c4a96e342f46
luke <lukehjgeorgeson@gmail.com>
2024-07-18 15:41:54 +0100

    Update

---
+++ interface.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import pandas as pd
+from main import Model
+
+if __name__ == '__main__':
+    if len(tf.config.experimental.list_physical_devices('GPU')):
+        device = "/GPU:0"
+    else:
+        device = "/CPU:0"
+    model = Model(device=device, data=pd.read_csv("./phishing_site_urls.csv"))
+
+    print(model.predict(input("Please enter your url: ")))
--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 import os
+from sklearn.utils import shuffle
 
 
 class Model:
-    def __init__(self, data, device):
+    def __init__(self, device, data=None):
         self.device = device
         data['Label'] = data["Label"].apply(lambda x: 1 if x == "bad" else 0)
+        data = shuffle(data)
+        print(data)
+
         self.maxLen = 100
         # Tokeniser
         self.tokeniser = tf.keras.preprocessing.text.Tokenizer()
@@ -15,15 +19,16 @@ class Model:
         self.sequences = self.tokeniser.texts_to_sequences(data["URL"])
         if os.path.exists("./model.keras"):
             self.model = tf.keras.models.load_model("./model.keras")
-        else:
+        elif data is not None:
             with tf.device(self.device):
                 self.model = tf.keras.models.Sequential([
-                    tf.keras.layers.Embedding(input_dim=len(self.tokeniser.word_index) + 1, output_dim=128,
-                                              input_length=self.maxLen),
+                    tf.keras.layers.Embedding(input_dim=len(self.tokeniser.word_index) + 1, output_dim=128),
                     tf.keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2),
                     tf.keras.layers.Dense(1, activation='sigmoid')
                 ])
                 self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        else:
+            raise Exception("No data provided/ no model found")
 
     def training(self, epochs):
         X = tf.keras.preprocessing.sequence.pad_sequences(self.sequences, maxlen=self.maxLen)
@@ -56,5 +61,5 @@ if __name__ == "__main__":
         device = "/CPU:0"
     print(device)
     data = pd.read_csv("./phishing_site_urls.csv")
-    model = Model(data, device)
+    model = Model(device, data)
     model.training(epochs=int(input("Enter how many epochs to train: ")))
